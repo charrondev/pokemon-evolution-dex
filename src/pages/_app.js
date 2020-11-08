cd9cf514591f86f88c50f8d65f8c831b0ff06fc4
Adam Charron <adam@charrondev.com>
2020-11-08 03:33:21 -0500

    Add analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,17 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    <>
+        <Component {...pageProps} />
+        <Head>
+            <script
+                async
+                defer
+                data-domain="evolution-dex.charron.dev"
+                src="https://stats.charron.dev/js/plausible.js"
+            ></script>
+        </Head>
+    </>;
 }
 
 export default MyApp;
