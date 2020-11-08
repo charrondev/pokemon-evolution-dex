83ab2216bf1239741c7bce613e6c76ca1350d877
Adam Charron <adam@charrondev.com>
2020-11-08 03:39:53 -0500

    add missing import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,17 +2,19 @@ import "../styles/globals.css";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-    <>
-        <Component {...pageProps} />
-        <Head>
-            <script
-                async
-                defer
-                data-domain="evolution-dex.charron.dev"
-                src="https://stats.charron.dev/js/plausible.js"
-            ></script>
-        </Head>
-    </>;
+    return (
+        <>
+            <Component {...pageProps} />
+            <Head>
+                <script
+                    async
+                    defer
+                    data-domain="evolution-dex.charron.dev"
+                    src="https://stats.charron.dev/js/plausible.js"
+                ></script>
+            </Head>
+        </>
+    );
 }
 
 export default MyApp;
